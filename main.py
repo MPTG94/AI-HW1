60834ba1eeaa609356ff5d40ff1d8b692690875f
drorco92 <57985817+drorco92@users.noreply.github.com>
2021-11-22 23:49:04 +0200

    Task 8 Finished

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ from Experiments import *
 
 
 if __name__ == "__main__":
-    test_robot(BreadthFirstSearchRobot, [0, 1, 2, 3, 4, 5])
-    # a = solve_and_display(BreadthFirstSearchRobot, 0)
+    # test_robot(BreadthFirstSearchRobot, [0, 1, 2, 3, 4, 5])
+    # test_robot(UniformCostSearchRobot, [0, 1, 2, 3, 4, 5])
+    # test_robot(WAStartRobot, [0, 1, 2, 3, 4, 5], heuristic=tail_manhattan_heuristic)
+    test_robot(WAStartRobot, [99], heuristic=tail_manhattan_heuristic)
+    # a = solve_and_display(UniformCostSearchRobot, 3)
+    # b = solve_and_display(BreadthFirstSearchRobot, 3)
